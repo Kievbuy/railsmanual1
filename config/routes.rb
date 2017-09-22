00983bccf8acf0b7e890414cbf0253f8ce28ed85
kievbuy <kievbuy@gmail.com>
2017-09-22 18:37:09 +0000

    Adв rout to the main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/index'
-
-  get 'page/index'
+  
+  root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
