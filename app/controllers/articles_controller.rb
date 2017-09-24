f36ab6325e884cc66bff39569afdd42b40cf1d1c
kievbuy <kievbuy@gmail.com>
2017-09-24 19:33:25 +0000

    Add basic authenticate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     
+    #http_basic_authenticate_with name: "kievbuy", password: "secret", except: [:index, :show]
+    
     def index
         @articles = Article.all
     end
